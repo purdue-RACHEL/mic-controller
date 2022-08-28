c8b4ad9083f5c0f5364405b448550f012e48c5ef
Micah <micah.morefield@gmail.com>
2022-08-28 15:15:15 -0400

    Initial Commit
    Initial Commit - Add sub_main.c - main loop for sub controller

---
+++ src/sub_main.c
@@ -0,0 +1,17 @@
+/*
+ * ECE 477 - R.A.C.H.E.L.
+ *
+ * sub_main.c - the main loop for sub controller
+ *
+ * Last edited by: Micah Morefield
+ * Last edit: 8/28/22
+ */
+
+#include "stm32f0xx.h"
+#include <stdio.h>
+
+
+int main()
+{
+    return 0;
+}
